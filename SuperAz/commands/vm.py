6ce7ba7c3134d77e0ecf4f1eaf8eacb688bcd27b
Johan Stenberg <johan.stenberg@microsoft.com>
2016-03-19 22:50:51 -0700

    Add example usage to commands

--- SuperAz/commands/vm.py
+++ SuperAz/commands/vm.py
@@ -5,9 +5,11 @@ def load_command_table():
 
 command_table = CommandTable()
 
-@command_table.option('--vm-name -name', dest='vm_name')
+@command_table.option('possie', metavar='da possie')
+@command_table.option('--vm-name -name', dest='vm_name', nargs='+')
+@command_table.option('--tags', dest='tags', nargs='+')
+@command_table.option('--resourcegroup', dest='resource_group', help='This is the help for resource group')
 @command_table.command('vm list-all')
 def vm_list_command(self):
     print('vm list command:')
     print(self)
-
